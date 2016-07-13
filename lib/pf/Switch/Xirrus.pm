07ef24a5812718627b76cb6324538ab0b99579fc
James Rouzier <jrouzier@inverse.ca>
2016-07-13 11:23:16 -0400

    Add trap normalizer for wlsxNUserEntryDeAuthenticatedTrapNormalizer

--- lib/pf/Switch/Xirrus.pm
+++ lib/pf/Switch/Xirrus.pm
@@ -70,6 +70,16 @@ sub supportsExternalPortal { return $TRUE; }
 sub supportsWebFormRegistration { return $TRUE; }
 
 
+#
+# %TRAP_NORMALIZERS
+# A hash of Xirrus trap normalizers
+# Use the following convention when adding a normalizer
+# <nameOfTrapNotificationType>TrapNormalizer
+#
+our %TRAP_NORMALIZERS = (
+   '.1.3.6.1.4.1.14823.2.3.1.11.1.2.1017' => 'wlsxNUserEntryDeAuthenticatedTrapNormalizer',
+);
+
 =item getVersion
 
 obtain image version information from switch
@@ -428,6 +438,22 @@ sub getAcceptForm {
     return $html_form;
 }
 
+
+=item wlsxNUserEntryDeAuthenticatedTrapNormalizer
+
+trap normalizer for wlsxNUserEntryDeAuthenticated trap
+
+=cut
+
+sub wlsxNUserEntryDeAuthenticatedTrapNormalizer {
+    my ($self, $trapInfo) = @_;
+    my $logger = $self->logger;
+    my ($pdu, $variables) = @$trapInfo;
+    return {
+        trapType => 'dot11Deauthentication',
+        trapMac => $self->getMacFromTrapVariablesForOIDBase($variables, '.1.3.6.1.4.1.14823.2.3.1.11.1.1.52.'),
+    };
+}
 
 =back
 
